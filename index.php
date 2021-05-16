ada255a0efb099ba3bf6dc9fbec752c64fec61e5
Gerard Urbański <biuro@internetprojects.pl>
2021-05-16 10:02:53 +0200

    wgranie do repozytorium

---
+++ index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<title>Nowe kwalifikacje nowy START - program Centro Plus Włocławek</title>
+	<meta name="description" content="Weź udział w bezpłatnych, certyfikowanych szkoleniach w zawodach: kelner/barman i spawacz. ">
+	<meta name="keywords" content="nowe kwalifikacje nowy start, kursy dla bezrobotnych, staże dla bezrobotnych, wspieranie zatrudnienia">
+	<meta name="generator" content="Internet Projects" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link href="/css/bootstrap.min.css" rel="stylesheet">
+
+	<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	<link href="https://fonts.googleapis.com/css?family=Anton&display=swap&subset=latin-ext" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Khand:300,400,700&amp;subset=latin-ext" rel="stylesheet">
+	<link href="/css/styles.css" rel="stylesheet">
+</head>
+
+<body>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116255609-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-116255609-1');
+	</script>
+	<div class="header projektMain vert">
+
+	</div>
+
+	<div id="sec1" class="projektIntro">
+		<div class="container">
+			<div class="row">
+				<h1>Nowe kwalifikacje nowy START</h1>
+				<h2><strong>TRZYMIESIĘCZNY PŁATNY STAŻ ZAWODOWY</strong></h2>
+				<div class="col-sm-12">
+					<p class="intro-lead">
+						bezpłatne, certyfikowane szkolenia w zawodach</p>
+					<ul class="intro-lead">
+						<li>KELNER / BARMAN</li>
+						<li>SPAWACZ</li>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="adresaciProjektu" id="sec2">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 text-center">
+					<h2>Adresaci projektu</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12 text-center">
+					<p>
+						Do udziału w projekcie zapraszamy mieszkańców Włocławka
+					</p>
+					<ul>
+						<li>w wieku 18-65 lat</li>
+						<li>bezrobotnych i/lub biernych zawodowo</li>
+						<li>kwalifikujących się lub korzystających ze świadczeń opieki społecznej</li>
+					</ul>
+					<ul>lub
+						<li>osoby posiadające orzeczenie o niepełnosprawności</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="projektMoreInfo" id="sec3">
+		<div class="vert">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<h2>Oferujemy:</h2>
+					</div>
+					<div class="col-sm-6">
+						<ul>
+							<li>Indywidualne Poradnictwo Zawodowe i Psychologiczne</li>
+							<li>Certyfikowane szkolenia w zawodzie
+								<ul>
+									<li>barman / kelner</li>
+									<li>spawacz</li>
+								</ul>
+							</li>
+							<li>Trzymiesięczny Staż Zawodowy</li>
+							<li>Indywidualne Pośrednictwo Pracy</li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-12 add-box">
+						<h2>10 uczestników zostanie poddanych metamorfozie<br>u profesjonalnego wizażysty</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-6">
+						<h2>Zapewniamy: </h2>
+					</div>
+					<div class="col-sm-6">
+						<ul>
+							<li>stypendium szkoleniowe do 8,90 zł brutto za 1 godzinę szkolenia.</li>
+							<li>stypendium stażowe 1334,44 brutto/miesięcznie</li>
+							<li>refundację kosztów opieki nad dzieckiem/osobą zależną</li>
+							<li>niezbędne badania lekarskie</li>
+							<li>ubezpieczenie NNW</li>
+							<li>catering</li>
+						</ul>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+	</div>
+
+	<div class="productSpecs" id="sec4">
+		<div class="container">
+			<div class="row">  
+					<div class="col-sm-6">
+						<h2 class="greenHeader">Dokumenty do pobrania: </h2>
+					</div>
+					<div class="col-sm-6">
+						<ul>
+							<li><a href="/repository/regulamin-uczestnictwa.pdf">Regulamin uczestnictwa w projekcie</a></li>
+							<li><a href="/repository/zal-1-formularz-rekrutacyjny.pdf">Formularz rekrutacyjny</a></li>
+							<li><a href="/repository/zal-2-oswiadczenie-o-akceptacji-regulaminu.pdf">Oświadczenie o akceptacji regulaminu</a></li>
+							<li><a href="/repository/zal-3-oswiadczenie-uczestnika.pdf">Oświadczenie uczestnika</a></li>
+							<li><a href="/repository/zal-4-test-motywacji.pdf">Test motywacji</a></li>
+							<li><a href="/repository/zal-5-oswiadczenie-o-statusie.pdf">Oświaczenie o statusie</a></li>
+							<li><a href="/repository/zal-6-deklaracja-udzialu-w-projekcie.pdf">Deklaracja udziału w projekcie</a></li>
+							<li><a href="/repository/zal-oswiadczenie-o-opiece-nad-dzieckiem-do-lat-6.pdf">Oświadczenie o opiece nad dzieckiem do lat 6</a></li>
+							
+							<li><a href="/repository/Oswiadczenie-osoby-pobierajacej-stypendium-szkoleniowe-stazowe.pdf">Oświadczenie osoby pobierającej stypendium szkoleniowe, stażowe</a></li>
+							<li><a href="/repository/Regulamin-przyznawania-i-wyplacania-stypendiow-szkoleniowych-i-stazowych.pdf">Regulamin przyznawania i wypłacania stypendiów szkoleniowych i stażowych</a></li>
+							<li><a href="/repository/Wniosek-o-refundacje-kosztow-opieki-nad-dzieckiem-do-lat-6.pdf">Wniosek o refundację kosztów opieki nad dzieckiem do lat 6</a></li>
+							<li><a href="/repository/Regulamin-refundacji-kosztow-opieki-nad-dzieckiem-do-lat-6.pdf">Regulamin refundacji kosztów opieki nad dzieckiem do lat 6</a></li>
+							<li><a href="/repository/Rozliczenie-faktycznie-poniesionych-kosztow-opieki-nad-dzieckiem-do-lat-6.pdf">Rozliczenie faktycznie poniesionych kosztów opieki nad dzieckiem do lat 6</a></li>
+							
+						</ul>
+						<hr>
+					</div>	
+						
+				</div>
+	
+		</div>
+	</div> 
+
+	<div class="projektMoreInfo" id="sec4">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h2 class="greenHeader">UDZIAŁ W PROJEKCIE JEST BEZPŁATNY</h2>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="contactInfo" id="sec5">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 text-center">
+					<h2>Szczegółowe informacje w Biurze Projektu: </h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-6">
+					<img class="img-responsive" src="/img/centroplus-logo.png" alt="Logo organizatora, firmy CentroPlus" />
+				</div>
+				<div class="col-sm-6">
+					<h3 class="row">
+						<span class="col-xs-2"><i class="fas fa-home"></i></span><span class="col-xs-10">Włocławek, ul. Jesionowa 1</span>
+					</h3>
+					<h3 class="row">
+						<span class="col-xs-2"><i class="fas fa-mobile-alt"></i></span><span class="col-xs-10">785 857 598</span>
+					</h3>
+					<h3 class="row">
+						<span class="col-xs-2"><i class="fas fa-envelope"></i></span><span class="col-xs-10">a.supel@centro-plus.pl</span>
+					</h3>
+					<h3 class="row">
+						<span class="col-xs-2"><i class="fas fa-link"></i></span><span class="col-xs-10">www.nowystart.centroplus.pl</span>
+					</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="projektLoga" id="sec6">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 lgd-info">
+					<p>Projekt "Nowe kwalifikacje nowy START" realizowany w ramach LSR na lata 2016-2023 LGD Włocławek, w ramach RPO Województwa Kujawsko-Pomorskiego na lata 2014-2020</p>
+				</div>
+				<div class="col-sm-4">
+					<p>Czas trwania projektu:</p>
+					<h3>02.2020 - 10.2020</h3>
+				</div>
+				<div class="col-sm-4">
+					<p>Wartość projektu:</p>
+					<h3>157 872,26 zł</h3>
+				</div>
+				<div class="col-sm-4">
+					<p>Wkład Funduszy Europejskich:</p>
+					<h3>149 977,26 zł</h3>
+				</div>
+			</div>
+			<hr>
+			<div class="row">
+				<div class="col-sm-12 text-center">
+					<img src="/img/nowy-start-loga.jpg" alt="Loga organizacji i podmiotów wspierających program." class="img-responsive">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- script references -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+	<script src="/js/bootstrap.min.js"></script>
+	<script src="/js/scripts.js"></script>
+	<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/solid.js" integrity="sha384-+Ga2s7YBbhOD6nie0DzrZpJes+b2K1xkpKxTFFcx59QmVPaSA8c7pycsNaFwUK6l" crossorigin="anonymous"></script>
+	<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js" integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script>
+</body>
+
+</html>
